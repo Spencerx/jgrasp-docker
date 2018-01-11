b70e7cc9689281e563c9d62df73dc47924cf7e50
Nick Mooney <nmooney@duosecurity.com>
2018-01-11 11:53:47 -0800

    add --rm flag to docker run so containers are removed upon shutdown

--- run.sh
+++ run.sh
@@ -3,4 +3,4 @@
 docker build -t "nickmooney/jgrasp" .
 ip=$(ifconfig en0 | grep inet | awk '$1=="inet" {print $2}')
 xhost + $ip
-docker run -it -e XAUTHORITY=/tmp/xauth -e DISPLAY=$ip:0 -v ~/.Xauthority:/tmp/xauth -v $PWD/src:/home/jgrasp/src -v $PWD/.grasp_settings:/home/jgrasp/.grasp_settings nickmooney/jgrasp $@
+docker run -it --rm -e XAUTHORITY=/tmp/xauth -e DISPLAY=$ip:0 -v ~/.Xauthority:/tmp/xauth -v $PWD/src:/home/jgrasp/src -v $PWD/.grasp_settings:/home/jgrasp/.grasp_settings nickmooney/jgrasp $@
